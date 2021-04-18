0aa7b6f4e19108799ca5806fd2b869a779c90c77
Arturo Cordoba <arturocv16@gmail.com>
2021-04-17 19:48:34 -0600

    Reemplazo de bloques en el directorio implementado

--- computer/directory/directory_control.py
+++ computer/directory/directory_control.py
@@ -1,18 +1,28 @@
 from computer.memory.memory_operation import MemoryOperation
+from computer.directory.directory import Directory
 
 
 class DirectoryControl:
     waiting = False
 
-    def __init__(self, p_buses, mem_bus):
+    def __init__(self, p_buses, mem_bus, update_buses):
         # Lista con los buses de los procesadores
         self.p_buses = p_buses
 
         # Bus para acceder a memoria
         self.mem_bus = mem_bus
 
+        self.update_buses = update_buses
+
+        self.associativity = 2
+        self.num_blocks = 4
+        self.num_sets = self.num_blocks / self.associativity
+
         # Estado de espera para cada procesador
         self.waiting_state = [False] * len(p_buses)
+
+        self.directory = Directory(self.num_blocks, len(p_buses))
+        self.cache = self.directory.cache
 
     def read_memory(self, mem_dir, node_id):
         # Se solicita la informacion a memoria principa;
@@ -23,3 +33,48 @@ class DirectoryControl:
     def write_memory(self, mem_dir, data):
         # Se solicita una operacion de escritura a memoria principal
         self.mem_bus.put([MemoryOperation.write, mem_dir, data])
+
+    def get_limits_set(self, index_set):
+        """
+        Funcion para obtener los limites que componen el set solicitado
+        :param index_set: numero del set solicitado
+        :return: [index inicial, index final + 1]
+        """
+        return [index_set * self.num_sets,
+                index_set * self.num_sets + self.associativity]
+
+    def get_index_min_references(self, x, y):
+        """
+        Funcion que retorna el index del bloque con menor cantidad de
+        referencias en un set
+        :param x: index del primer elemento del set
+        :param y: index del ultimo elemento del set + 1
+        :return: index del bloque con menor cantidad de referencias
+        """
+        # Se establece un valor que va a ser mayor a la cantidad
+        # de referencias posible
+        min_references = len(self.p_buses) + 1
+        index = x
+
+        # Se recorre el set buscando el bloque con menos referencias
+        for i in range(x, y):
+            # Se obtiene la cantidad total de referencias del bloque
+            block_references = sum(self.directory.processorRef[i])
+            if block_references < min_references:
+                min_references = block_references
+                index = i
+
+        return index
+
+    def replaceBlock(self, mem_dir, data, newState, node_id):
+        # Se obtiene el index del set en el que debe estar el bloque
+        index_set = int(mem_dir % self.num_sets)
+
+        # Se obtienen los limites del set
+        x, y = self.get_limits_set(index_set)
+
+        # Se obtiene el index del bloque a reemplazar
+        index = self.get_index_min_references(x, y)
+
+        # Se escribe la nueva informacion en el bloque
+        self.directory.write(node_id, mem_dir, data, index, newState, self.update_buses)
--- testcomputer/test_directory_control.py
+++ testcomputer/test_directory_control.py
@@ -1,6 +1,7 @@
 import unittest
 from queue import Queue
 from computer.directory.directory_control import DirectoryControl
+from computer.directory.directory_state import DirectoryState
 from computer.memory.memory_operation import MemoryOperation
 
 
@@ -8,10 +9,33 @@ def get_directory_control():
     mem_bus = Queue()
     num_processors = 4
     p_buses = []
+    update_buses = []
     for i in range(0, num_processors):
         p_buses.append([Queue(), Queue()])
+        p_buses.append(Queue())
 
-    return DirectoryControl(p_buses, mem_bus)
+    directory_control = DirectoryControl(p_buses, mem_bus, update_buses)
+    directory = directory_control.directory
+
+    """
+    | N | Dir | Data | Sta | P    |
+    | 0 |  10 |  11  | DI  | 0000 |
+    | 1 |  20 |  21  | DS  | 0110 |
+    | 2 |  30 |  31  | DM  | 1000 |
+    | 3 |  40 |  41  | DS  | 1011 |
+    """
+    directory.cache.blockDirMem = [10, 20, 30, 40]
+    directory.cache.blockData = [11, 21, 31, 41]
+    directory.blockState = [DirectoryState.uncached,
+                            DirectoryState.shared,
+                            DirectoryState.exclusive,
+                            DirectoryState.shared]
+    directory.processorRef = [[0, 0, 0, 0],
+                              [0, 1, 1, 0],
+                              [1, 0, 0, 0],
+                              [1, 0, 1, 1]]
+
+    return directory_control
 
 
 class TestDirectoryControlMethods(unittest.TestCase):
@@ -46,3 +70,36 @@ class TestDirectoryControlMethods(unittest.TestCase):
 
         for i in range(0, len(waiting_state)):
             self.assertFalse(waiting_state[i])
+
+    def test_get_limits_set(self):
+        directory = get_directory_control()
+
+        index_set = 0
+        x, y = directory.get_limits_set(index_set)
+        self.assertEqual(x, 0)
+        self.assertEqual(y, 2)
+
+        index_set = 1
+        x, y = directory.get_limits_set(index_set)
+        self.assertEqual(x, 2)
+        self.assertEqual(y, 4)
+
+    def test_get_index_min_references(self):
+        directory = get_directory_control()
+        """
+        | N | Dir | Data | Sta | P    |
+        | 0 |  10 |  11  | DI  | 0000 |
+        | 1 |  20 |  21  | DS  | 0110 |
+        | 2 |  30 |  31  | DM  | 1000 |
+        | 3 |  40 |  41  | DS  | 1010 |
+        """
+        x1 = 0
+        y1 = 2
+        x2 = 2
+        y2 = 4
+
+        i = directory.get_index_min_references(x1, y1)
+        self.assertEqual(i, 0)
+
+        i = directory.get_index_min_references(x2, y2)
+        self.assertEqual(i, 2)
